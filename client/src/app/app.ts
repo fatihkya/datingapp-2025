2dfe6ef21581907e01ca3f0178d472282e75e4f5
Fatih <fatih-25-kaya@hotmail.com>
2025-12-10 17:20:10 +0300

    cliend 3

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,18 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  public readonly title = signal('client');
+export class App implements OnInit {
+
+  private http = inject(HttpClient)
+  public readonly title = 'Dating App';
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/members").subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log('Complated')
+      
+    })
+  }
+
 }
